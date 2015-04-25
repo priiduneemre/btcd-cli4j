b10e3942728ccc846d086d7bff82fb8b9a8c8713
Priidu Neemre <priidu.neemre@gmail.com>
2015-04-25 10:17:37 +0300

    Add enum with supported 'bitcoind' notification types (ALERT, BLOCK, WALLET)

---
+++ core/src/main/java/com/neemre/btcdcli4j/core/daemon/Notifications.java
@@ -0,0 +1,17 @@
+package com.neemre.btcdcli4j.core.daemon;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+/**An enumeration specifying the <i>bitcoind</i> notification types currently supported by 
+ * btcd-cli4j.**/
+@Getter
+@ToString
+@AllArgsConstructor
+public enum Notifications {
+
+	ALERT,
+	BLOCK,
+	WALLET;
+}
