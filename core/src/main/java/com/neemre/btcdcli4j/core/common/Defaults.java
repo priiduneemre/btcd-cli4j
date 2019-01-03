6b33ad8168ebeddaec988b3c87fa7e3d2f0aa819
kushal <kushal@bitso.com>
2019-01-03 10:51:29 -0800

    Add BSV version

--- core/src/main/java/com/neemre/btcdcli4j/core/common/Defaults.java
+++ core/src/main/java/com/neemre/btcdcli4j/core/common/Defaults.java
@@ -12,6 +12,7 @@ public final class Defaults {
 	public static final int DECIMAL_SCALE = 8;
 	public static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_UP;
 
-	public static final String[] NODE_VERSIONS = {"0.10.0", "0.10.1", "0.10.2", "0.10.3", "0.15.0", "0.16.0", "0.16.1", "0.16.2"};
+	public static final String[] NODE_VERSIONS = {"0.10.0", "0.10.1", "0.10.2", "0.10.3", "0.15.0", "0.16.0", "0.16.1", "0.16.2", //btc and friends
+			"0.0.10"}; //bsv
 	public static final String JSON_RPC_VERSION = "1.0";
 }
